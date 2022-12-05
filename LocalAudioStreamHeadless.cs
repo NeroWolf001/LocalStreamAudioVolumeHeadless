47ad786667a5509204d433fe8edabb5bf2ad5778
NeroWolf001 <118631622+NeroWolf001@users.noreply.github.com>
2022-12-04 20:33:49 -0500

    NML Fix

--- LocalAudioStreamHeadless.cs
+++ LocalAudioStreamHeadless.cs
@@ -11,20 +11,21 @@ namespace LocalAudioStreamVolumeHeadless
         public override string Author => "NeroWolf & LeCloutPanda";
         public override string Version => "1.0.0";
 
+        public static ModConfiguration config;
+
         [AutoRegisterConfigKey]
-        private static ModConfigurationKey<bool> ENABLED = new ModConfigurationKey<bool>("Set user audio stream volume to 0 in hosted sessions.");
+        private static ModConfigurationKey<bool> ENABLED = new ModConfigurationKey<bool>("Set user audio stream volume to 0 in hosted sessions.", "", () => true);
 
         public override void OnEngineInit()
         {
-            LocalAudioStreamVolumeHeadless.config = base.GetConfiguration();
-            LocalAudioStreamVolumeHeadless.config.Save(true);
+            config = base.GetConfiguration();
+            config.Save(true);
 
             Harmony harmony = new Harmony($"dev.{Author}.{Name}");
             harmony.PatchAll();
 
             Engine.Current.OnReady += Current_OnReady;
         }
-        public static ModConfiguration config;
         private void Current_OnReady()
         {
             Engine.Current.WorldManager.WorldAdded += WorldAdded;
@@ -42,20 +43,21 @@ namespace LocalAudioStreamVolumeHeadless
 
             if (arg2.GetType() == typeof(UserAudioStream<StereoSample>))
             {
-                AudioOutput audioOutput = arg1.GetComponent<AudioOutput>();
+                    AudioOutput audioOutput = arg1.GetComponent<AudioOutput>();
 
-                if (audioOutput == null) return;
+                    if (audioOutput == null) return;
 
-                ValueUserOverride<float> userOverride = audioOutput.Volume.OverrideForUser<float>(arg1.World.HostUser, 0);
-                userOverride.CreateOverrideOnWrite.Value = true;
-                userOverride.Default.Value = 0;
-                Slot Handle = arg1.FindChild(ch => ch.Name.Equals("Handle"), 1);
-                if (Handle.FindChild(ch => ch.Name.Equals("Local Text"), 1) != null) return;
+                    ValueUserOverride<float> userOverride = audioOutput.Volume.OverrideForUser<float>(arg1.World.HostUser, 0);
+                    userOverride.CreateOverrideOnWrite.Value = true;
+                    userOverride.Default.Value = 0;
 
-                TextRenderer text = Handle.AddSlot("Local Text").AttachComponent<TextRenderer>();
-                text.Text.Value = "Local Audio";
-                text.Slot.Scale_Field.Value = new BaseX.float3(0.3f, 0.3f, 0.3f);
-                text.Slot.Position_Field.Value = new BaseX.float3(0f, 0f, -0.0075f);
+                    Slot Handle = arg1.FindChild(ch => ch.Name.Equals("Handle"), 1);
+                    if (Handle.FindChild(ch => ch.Name.Equals("Local Text"), 1) != null) return;
+
+                    TextRenderer text = Handle.AddSlot("Local Text").AttachComponent<TextRenderer>();
+                    text.Text.Value = "Local Audio";
+                    text.Slot.Scale_Field.Value = new BaseX.float3(0.3f, 0.3f, 0.3f);
+                    text.Slot.Position_Field.Value = new BaseX.float3(0f, 0f, -0.0075f);
             }
         }
     }
